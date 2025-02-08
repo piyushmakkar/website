568722c993a0a62d7e61dd0e73142efadc65b605
piyushmakkar <piyushmakkar2004@gmail.com>
2025-02-08 20:56:42 +0530

    Updates in css

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -6,20 +6,20 @@ const Footer = () => {
     <footer id="contact" className="footer-section py-5">
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
+          <div className="col-md-1">
             <h6 className="fw-bold">Industries we cater to</h6>
-            <ul className="list-unstyled mt-3">
+            <ul className="mt-3">
               <li>
-              Mechanical Engineering Manager  
+              Mechanical  
               </li>
               <li>
-              Oil & Gas Industries
+              Oil & Gas 
               </li>
               <li>
-              Electronics Manufacturing
+              Electronic 
               </li>
               <li>
-              Electrical Engineering
+              Electrical 
               </li>
               <li>
               Automotive
@@ -27,11 +27,17 @@ const Footer = () => {
               <li>
               Pharmaceuticals
               </li>
+              <li>
+              Chemical
+              </li>
+              <li>
+              Paint
+              </li>
             </ul>
           </div>
-          <div className="col-md-2">
+          <div className="col-md-3">
             <h6 className="fw-bold">PRODUCT</h6>
-            <ul className="list-unstyled mt-3">
+            <ul className="mt-3">
             <li>Pallets</li>
             <li>Wooden Boxes</li>
             <li>Tapes</li>
@@ -40,16 +46,20 @@ const Footer = () => {
             </ul>
           </div>
           <div className="col-md-3">
-            <h6 className="fw-bold">Socials</h6>
+            <h6 className="fw-bold">Address</h6>
             <ul className="list-unstyled mt-3">
-
+            <li>
+              1/21, Horizon Industrial Park,<br></br>
+              At Post Bamangam, Tal. Karjan, <br></br>
+              Dist. Vadodara - 391243, Gujarat (India)
+            </li>
             </ul>
           </div>
           <div className="col-md-3">
             <h6 className="fw-bold">Contact us</h6>
             <ul className="list-unstyled mt-3">
               <li><a href="#">Phone : +91 816092116</a></li>
-              <li><a href="#">Email : alkaindustries83@gmail.com</a></li>
+              <li><a href="#contact">Email : enquiry@alkaindustries.co.in</a></li>
             </ul>
             <form className="newsletter-form mt-3">
                 <input
@@ -69,14 +79,6 @@ const Footer = () => {
           </div>
         </div>
         <hr className="my-4" />
-
-        <div className="row">
-          <div className="col-md-6">
-          </div>
-          <div className="col-md-6 text-md-end">
-            <p className="text-muted mb-0">© 2025 Piyush. All Rights Reserved.</p>
-          </div>
-        </div>
       </div>
     </footer>
   );
